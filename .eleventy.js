d1532085a4cb661dd8843581c755d656703c753a
camphan <camphan@camphans-MacBook-Pro.local>
2020-11-25 16:56:30 +0700

    minify source

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,37 @@
+const htmlmin = require('html-minifier');
+const lazyImagesPlugin = require('eleventy-plugin-lazyimages');
+
 module.exports = function (config) {
+
+	config.addPlugin(lazyImagesPlugin, {
+		transformImgPath: (imgPath) => {
+			if (imgPath.startsWith('/assets/imgs') && !imgPath.startsWith('//')) {
+				return `./src${imgPath}`;
+			}
+
+			return imgPath;
+		},
+	});
+
+	// html minify
+	if (process.env.NODE_ENV == 'production') {
+		config.addTransform('htmlmin', function (content, outputPath) {
+			if (outputPath.endsWith('.html')) {
+				let minified = htmlmin.minify(content, {
+					useShortDoctype: true,
+					removeComments: true,
+					collapseWhitespace: true,
+					minifyjs: { quote_style: 1 },
+					preventAttributesEscaping: true,
+					quoteCharacter: "'",
+				});
+				return minified;
+			}
+
+			return content;
+		});
+	}
+
 	config.addPassthroughCopy('src/assets/imgs');
 	return {
 		dir: {
--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,59 @@
-const gulp = require('gulp');
-const browsersync = require('browser-sync').create();
-const isProd = process.env.NODE_ENV === 'production';
-const uglify = require('gulp-uglify');
-const postcss = require('gulp-postcss');
+const gulp = require("gulp");
+const browsersync = require("browser-sync").create();
+const isProd = process.env.NODE_ENV === "production";
+const uglify = require("gulp-uglify");
+const postcss = require("gulp-postcss");
+const cssnano = require("cssnano")({
+	preset: "default",
+	discardComments: { removeAll: true },
+});
+const purgecss = require("@fullhuman/postcss-purgecss")({
+	// Specify the paths to all of the template files in your project
+	content: ["./src/**/*.liquid"],
+	whitelist: ["active", "fade-out", "loading"],
+	whitelistPatterns: [/swiper/g],
+	whitelistPatternsChildren: [/swiper/g],
+	// Include any special characters you're using in this regular expression
+	defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
 
 // browsersync server
 function server(done) {
-	browsersync.init({
-		server: './_site/',
-		files: ['./_site'],
-		port: 3000,
-		open: false,
-	});
-	done();
+  browsersync.init({
+    server: "./_site/",
+    files: ["./_site"],
+    port: 3000,
+    open: false,
+  });
+  done();
 }
 
 // build js
 function jsBuild() {
-	return gulp
-		.src('./src/assets/js/main.js', { sourcemaps: true })
-		.pipe(uglify())
-		.pipe(gulp.dest('./_site/assets/js/', { sourcemaps: true }));
+  return gulp
+    .src("./src/assets/js/main.js", { sourcemaps: true })
+    .pipe(uglify())
+    .pipe(gulp.dest("./_site/assets/js/", { sourcemaps: true }));
 }
 
 // build styles
 function stylesBuild() {
-	return gulp
-		.src('./src/assets/css/main.css')
-		.pipe(postcss([require('autoprefixer'), require('tailwindcss')]))
-		.pipe(gulp.dest('./_site/assets/css/'));
+  return gulp
+    .src("./src/assets/css/main.css")
+    .pipe(
+      postcss([
+        require("autoprefixer"),
+        require("tailwindcss"),
+        ...(process.env.NODE_ENV === "production" ? [purgecss, cssnano] : []),
+      ])
+    )
+    .pipe(gulp.dest("./_site/assets/css/"));
 }
 
 // watch all files
 function watchFiles() {
-	gulp.watch('./src/assets/css/**/*', stylesBuild);
-	gulp.watch('./src/assets/js/**/*', jsBuild);
+  gulp.watch("./src/assets/css/**/*", stylesBuild);
+  gulp.watch("./src/assets/js/**/*", jsBuild);
 }
 
 const build = gulp.series(gulp.parallel(stylesBuild, jsBuild));
