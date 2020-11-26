e0a9e7a59bb7bb0fc098f79e468670f36a330bc8
cam phan <cam.phan12993@gmail.com>
2020-11-26 21:17:32 +0700

    update js

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,31 +1,31 @@
 var swiper = new Swiper('.swiper-container', {
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
 });
 
 function toggleDarkMode() {
-  var isDark = document.querySelector('body').classList.contains('theme-dark');
-  updateStatus(isDark);
+	var isDark = document.querySelector('body').classList.contains('theme-dark');
+	var icon = document.querySelector('#toggleBtn ion-icon');
+	if (isDark) {
+		document.querySelector('body').classList.remove('theme-dark');
+		icon.setAttribute('name', 'moon-outline');
+		localStorage.setItem('mode', 'light');
+	} else {
+		document.querySelector('body').classList.add('theme-dark');
+		icon.setAttribute('name', 'sunny-outline');
+		localStorage.setItem('mode', 'dark');
+	}
 }
 
-function updateStatus(isDark) {
-  var icon = document.querySelector('#toggleBtn ion-icon');
-  if(isDark) {
-    document.querySelector('body').classList.remove('theme-dark');
-    icon.setAttribute('name', 'moon-outline');
-    localStorage.setItem('mode', 'light');
-  } else {
-    document.querySelector('body').classList.add('theme-dark');
-    icon.setAttribute('name', 'sunny-outline');
-    localStorage.setItem('mode', 'dark');
-  }
-}
-
-window.onload = function(e) {
-  updateStatus(localStorage.getItem('mode') === 'light');
-  setTimeout(function(){
+window.onload = function (e) {
+	var icon = document.querySelector('#toggleBtn ion-icon');
+	if (localStorage.getItem('mode') === 'dark') {
+		document.querySelector('body').classList.add('theme-dark');
+		icon.setAttribute('name', 'sunny-outline');
+	}
+	setTimeout(function () {
 		document.getElementById('loading').classList.add('fade-out');
 	}, 2000);
-}
+};
