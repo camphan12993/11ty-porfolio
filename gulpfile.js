7f4c9c7503eed908c4f295ccba28804bf01cdb3f
camphan12993 <cam.phan12993@gmail.com>
2021-01-14 13:06:18 +0700

    fix pk.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,16 @@
-const gulp = require("gulp");
-const browsersync = require("browser-sync").create();
-const isProd = process.env.NODE_ENV === "production";
-const uglify = require("gulp-uglify");
-const postcss = require("gulp-postcss");
-const postcssimport = require("postcss-import");
-const cssnano = require("cssnano")({
-  preset: "default",
+const gulp = require('gulp');
+const browsersync = require('browser-sync').create();
+const uglify = require('gulp-uglify');
+const postcss = require('gulp-postcss');
+const postcssimport = require('postcss-import');
+const cssnano = require('cssnano')({
+  preset: 'default',
   discardComments: { removeAll: true },
 });
-const purgecss = require("@fullhuman/postcss-purgecss")({
+const purgecss = require('@fullhuman/postcss-purgecss')({
   // Specify the paths to all of the template files in your project
-  content: ["./src/**/*.liquid"],
-  safelist: ["active", "fade-out", "loading", "theme-dark",/swiper/g],
+  content: ['./src/**/*.liquid'],
+  safelist: ['active', 'fade-out', 'loading', 'theme-dark', /swiper/g],
   // Include any special characters you're using in this regular expression
   defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
 });
@@ -19,8 +18,8 @@ const purgecss = require("@fullhuman/postcss-purgecss")({
 // browsersync server
 function server(done) {
   browsersync.init({
-    server: "./_site/",
-    files: ["./_site"],
+    server: './_site/',
+    files: ['./_site'],
     port: 3000,
     open: false,
   });
@@ -30,13 +29,13 @@ function server(done) {
 // build js
 function jsBuild() {
   return gulp
-    .src("./src/assets/js/main.js", {
-      sourcemaps: process.env.NODE_ENV !== "production",
+    .src('./src/assets/js/main.js', {
+      sourcemaps: process.env.NODE_ENV !== 'production',
     })
     .pipe(uglify())
     .pipe(
-      gulp.dest("./_site/assets/js/", {
-        sourcemaps: process.env.NODE_ENV !== "production",
+      gulp.dest('./_site/assets/js/', {
+        sourcemaps: process.env.NODE_ENV !== 'production',
       })
     );
 }
@@ -44,26 +43,26 @@ function jsBuild() {
 // build styles
 function stylesBuild() {
   return gulp
-    .src("./src/assets/css/main.css")
+    .src('./src/assets/css/main.css')
     .pipe(
       postcss([
         postcssimport(),
-        require("tailwindcss")('./tailwind.config.js'),
-        require("autoprefixer"),
-        ...(process.env.NODE_ENV === "production" ? [purgecss, cssnano] : []),
+        require('tailwindcss')('./tailwind.config.js'),
+        require('autoprefixer'),
+        ...(process.env.NODE_ENV === 'production' ? [purgecss, cssnano] : []),
       ])
     )
-    .pipe(gulp.dest("./_site/assets/css/"));
+    .pipe(gulp.dest('./_site/assets/css/'));
 }
 
 // watch all files
 function watchFiles() {
-  gulp.watch("./src/assets/css/**/*", stylesBuild);
-  gulp.watch("./src/assets/js/**/*", jsBuild);
+  gulp.watch('./src/assets/css/**/*', stylesBuild);
+  gulp.watch('./src/assets/js/**/*', jsBuild);
 }
 
 const build = gulp.series(gulp.parallel(stylesBuild, jsBuild));
-const watch = gulp.series(build,gulp.parallel(server, watchFiles));
+const watch = gulp.series(build, gulp.parallel(server, watchFiles));
 
 exports.build = build;
 exports.watch = watch;
